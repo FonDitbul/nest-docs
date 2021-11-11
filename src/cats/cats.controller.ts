09ac965e14149e203f6b9b57bb6fbb1c2d082632
FonDitbul <niki7084@naver.com>
2021-11-11 16:48:02 +0900

    cats providers service

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -8,8 +8,12 @@ import {
     Post,
 } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+
 @Controller('cats')
 export class CatsController {
+    constructor(private catsService: CatsService) {}
+
     @Post()
     crete(@Body() createCatDto: CreateCatDto): string {
         return 'This action adds a new cat';
